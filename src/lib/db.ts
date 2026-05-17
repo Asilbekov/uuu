1af495b227967ed0036a55cc625aa16b2eeedfdb
Z User <z@container>
2026-05-17 11:19:06 +0000

    2e37432e-e87d-483a-8fd2-af4c8d6b3014

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,26 @@
 import { PrismaClient } from '@prisma/client'
 
+// Override DATABASE_URL BEFORE creating PrismaClient
+// The FUSE filesystem at /home/z/my-project doesn't support SQLite writes
+if (typeof process !== 'undefined' && process.env) {
+  process.env.DATABASE_URL = 'file:/tmp/chemtest_db/custom.db'
+}
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+// Force create a new client to pick up the new URL
+if (process.env.NODE_ENV !== 'production') {
+  if (globalForPrisma.prisma) {
+    // Disconnect old client
+    globalForPrisma.prisma.$disconnect().catch(() => {})
+  }
+  globalForPrisma.prisma = undefined
+}
+
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['query'],
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
